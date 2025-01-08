1b2596e3349935d4639fc729a68942f7da00632c
Yash <yashshah@gmail.com>
2025-01-08 13:25:38 +0530

    UC-5 above 100 added resolved

--- snakeladdergame.java
+++ snakeladdergame.java
@@ -6,18 +6,20 @@ public class SnakeLadderGame {
 
         int posFirstPlayer = 0;
         boolean currTurn = true;
-        while(posFirstPlayer != 100){
-            if(currTurn == true){
+        while (posFirstPlayer != 100) {
+            if (currTurn) {
                 System.out.println("Player 1 is currently at the Position: " + posFirstPlayer);
+
                 // UC - 2
                 // random function used
                 int dieRolled = (int) (Math.random() * 6 + 1);
+
                 // UC - 3
                 System.out.println("Dice rolls by: " + dieRolled);
+
                 // using random to get option
                 int Option = (int) (Math.random() * 3 + 1);
-                switch(Option)
-                {
+                switch (Option) {
                     case 1:
                         System.out.println("NO PLAY: " + posFirstPlayer);
                         System.out.println("Player stays at the same position");
@@ -33,16 +35,20 @@ public class SnakeLadderGame {
                         posFirstPlayer = posFirstPlayer - dieRolled;
                         break;
                 }
-                if(posFirstPlayer < 0){
+
+                if (posFirstPlayer < 0) {
                     posFirstPlayer = 0;
                 }
+
                 // UC - 5 Exact 100
-                if(posFirstPlayer > 100){
+                if (posFirstPlayer > 100) {
                     posFirstPlayer -= dieRolled;
                 }
+
                 System.out.println();
             }
         }
+
         System.out.println("Player 1 has reached the winning Position 100");
     }
 }
