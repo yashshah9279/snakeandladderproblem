02aa73e8fe7898d97d3176d361da2e4a96c70c9c
Yash <yashshah@gmail.com>
2025-01-08 13:21:16 +0530

    UC-4 repeat till winning done

--- snakeladdergame.java
+++ snakeladdergame.java
@@ -4,30 +4,41 @@ public class SnakeLadderGame {
         System.out.println("Welcome to the Snake and Ladder Game!");
         System.out.println("Single Player at Start Position 0");
 
-        // UC - 2
-        // random function used
-        int dieRolled = (int) (Math.random() * 6 + 1);
-        System.out.println("Player 1 rolled the die and got: " + dieRolled);
-
-        // UC - 3
-        System.out.println("Dice rolls by: " + dieRolled);
-        Scanner sc = new Scanner(System.in);
-        // using random to get option
-        int Option = (int) (Math.random() * 2 + 1);
-        int position = 0;
-        switch(Option)
-        {
-            case 1:
-                System.out.println("NO PLAY: " + position);
-                System.out.println("Player stays at the same position");
-                break;
-            case 2:
-                System.out.println("LADDER: " + (dieRolled + position));
-                System.out.println("Player moves ahead by: " + dieRolled);
-                break;
-            case 3:
-                System.out.println("SNAKE: " + (dieRolled + position));
-                System.out.println("Player moves behind by: " + dieRolled);
+        int posFirstPlayer = 0;
+        boolean currTurn = true;
+        while(posFirstPlayer != 100){
+            if(currTurn == true){
+                System.out.println("Player 1 is currently at the Position: " + posFirstPlayer);
+                // UC - 2
+                // random function used
+                int dieRolled = (int) (Math.random() * 6 + 1);
+                // UC - 3
+                System.out.println("Dice rolls by: " + dieRolled);
+                // using random to get option
+                int Option = (int) (Math.random() * 3 + 1);
+                switch(Option)
+                {
+                    case 1:
+                        System.out.println("NO PLAY: " + posFirstPlayer);
+                        System.out.println("Player stays at the same position");
+                        break;
+                    case 2:
+                        System.out.println("LADDER: " + (dieRolled + posFirstPlayer));
+                        System.out.println("Player moves ahead by: " + dieRolled);
+                        posFirstPlayer = posFirstPlayer + dieRolled;
+                        break;
+                    case 3:
+                        System.out.println("SNAKE: " + (dieRolled + posFirstPlayer));
+                        System.out.println("Player moves behind by: " + dieRolled);
+                        posFirstPlayer = posFirstPlayer - dieRolled;
+                        break;
+                }
+                if(posFirstPlayer < 0){
+                    posFirstPlayer = 0;
+                }
+                System.out.println();
+            }
         }
+        System.out.println("Player 1 has reached the winning Position 100");
     }
 }
