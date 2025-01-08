2b8a730882e9a150b8ed7dc968b8a81e0311936c
Yash <yashshah@gmail.com>
2025-01-08 13:17:58 +0530

    UC-3 play options added

--- snakeladdergame.java
+++ snakeladdergame.java
@@ -8,5 +8,26 @@ public class SnakeLadderGame {
         // random function used
         int dieRolled = (int) (Math.random() * 6 + 1);
         System.out.println("Player 1 rolled the die and got: " + dieRolled);
+
+        // UC - 3
+        System.out.println("Dice rolls by: " + dieRolled);
+        Scanner sc = new Scanner(System.in);
+        // using random to get option
+        int Option = (int) (Math.random() * 2 + 1);
+        int position = 0;
+        switch(Option)
+        {
+            case 1:
+                System.out.println("NO PLAY: " + position);
+                System.out.println("Player stays at the same position");
+                break;
+            case 2:
+                System.out.println("LADDER: " + (dieRolled + position));
+                System.out.println("Player moves ahead by: " + dieRolled);
+                break;
+            case 3:
+                System.out.println("SNAKE: " + (dieRolled + position));
+                System.out.println("Player moves behind by: " + dieRolled);
+        }
     }
 }
