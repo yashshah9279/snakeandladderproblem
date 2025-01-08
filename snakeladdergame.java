d761c3f0470de5800eebe15dd3096e715233040a
Yash <yashshah@gmail.com>
2025-01-08 13:06:49 +0530

    Start at UC-1

---
+++ snakeladdergame.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class SnakeLadderGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Game!");
+        System.out.println("Single Player at Start Position 0");
+    }
+}
